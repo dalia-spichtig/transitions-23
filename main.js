9426b73ea0bcc8ffcd85ab328480d241f4c3b7c2
dalia-spichtig <144672896+dalia-spichtig@users.noreply.github.com>
2023-12-08 14:01:07 +0100

    v2

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 import { runSequence } from "./shared/sequenceRunner.js";
 
 const emptySequence = [
-    "sketches/example-sequence-empty",
-    "sketches/example-sequence-empty",
+    "sketches/example-sequence-empty-1",
+    "sketches/example-sequence-empty-2",
 ]
 
 const exampleSequence = [
-    "sketches/example-sequence-1",
-    "sketches/example-sequence-2",
-    "sketches/example-sequence-3"
+    "sketches/dalia-day1",
+    "sketches/dalia-day2",
+    "sketches/dalia-day3",
+    "sketches/dalia-day4"
 ]
 
 runSequence(exampleSequence)
